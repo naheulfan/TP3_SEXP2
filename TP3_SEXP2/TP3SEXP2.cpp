8c21f28f5f638125720acc7d72f774b7ae634bce
naheulfan <brunopleau@hotmail.com>
2017-11-28 15:31:15 -0500

    First commit, test git

--- TP3_SEXP2/TP3SEXP2.cpp
+++ TP3_SEXP2/TP3SEXP2.cpp
@@ -1,11 +1,21 @@
 // TP3SEXP2.cpp : Définit le point d'entrée pour l'application console.
 //
-
+#include <iostream>
 #include "stdafx.h"
+#include <thread>
 
-
+using namespace std;
 int main()
 {
+	thread first(Test);
+	
+	while (true)
+	{
+	}
     return 0;
+}
+void Test()
+{
+
 }
 
