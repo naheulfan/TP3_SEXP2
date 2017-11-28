fe73aeeb5ec9078f948c3f276fa4f8016ad23e80
naheulfan <brunopleau@hotmail.com>
2017-11-28 16:12:56 -0500

    STUFF ABOUT IOSTREAM AND SHIT

--- TP3_SEXP2/TP3SEXP2.cpp
+++ TP3_SEXP2/TP3SEXP2.cpp
@@ -1,16 +1,22 @@
 // TP3SEXP2.cpp : Définit le point d'entrée pour l'application console.
 //
-#include <iostream>
 #include "stdafx.h"
 #include <thread>
-
+#include <string>
+#include <iostream>
 using namespace std;
 int main()
 {
-	thread first(Test);
-	
-	while (true)
+	//while (true)
 	{
+		std::cout << "Voulez vous vous connecter à un site web? [y/n]" << endl;
+		char value;
+		cin >> value;
+		if (value == 'y')
+		{
+			cout << "Veuillez entrer une adresse de connexion valide" << endl;
+			
+		}
 	}
     return 0;
 }
